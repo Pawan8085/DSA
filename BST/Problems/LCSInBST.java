33bcc63c2783037392a0862e99c0963426a53333
Pawan <kumarpawanm8085@gmail.com>
2025-11-27 22:46:03 +0530

    LCS in BST

---
+++ BST/Problems/LCSInBST.java
@@ -0,0 +1,18 @@
+public class LCSInBST {
+   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // base case
+        if(root == null) return null;
+
+        if(p.val < root.val && q.val < root.val){
+
+            return lowestCommonAncestor(root.left, p, q);
+
+        }else if(p.val > root.val && q.val > root.val){
+
+            return lowestCommonAncestor(root.right, p, q);
+
+        }
+
+        return root;
+    }
+}
