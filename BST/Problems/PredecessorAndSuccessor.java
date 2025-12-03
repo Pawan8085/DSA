4d78d4acce47166669f1537c0dcb219549d1254a
Pawan <kumarpawanm8085@gmail.com>
2025-12-03 22:55:20 +0530

    BST Predecessor and Successor

---
+++ BST/Problems/PredecessorAndSuccessor.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+import org.w3c.dom.Node;
+
+public class PredecessorAndSuccessor {
+    
+    public ArrayList<Node> findPreSuc(Node root, int key) {
+        
+        
+        Node p = null;
+        Node cur = root;
+        
+        while(cur != null){
+            if(cur.data < key){
+                p = cur;
+                cur = cur.right;
+            }else{
+                cur = cur.left;
+            }
+        }
+        
+        cur = root;
+        Node s = null;
+        while(cur != null){
+            if(cur.data > key){
+                s = cur;
+                cur = cur.left;
+            }else{
+                cur = cur.right;
+            }
+        }
+        
+        ArrayList<Node> res = new ArrayList<>();
+        res.add(p);
+        res.add(s);
+        return res;
+    }
+}
