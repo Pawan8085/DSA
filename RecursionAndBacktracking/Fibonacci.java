971586d3e54ce2effe4bb3c7d5cdc2f7dd42213c
Pawan <kumarpawanm8085@gmail.com>
2025-12-10 20:36:08 +0530

    Fibonacci recursion

---
+++ RecursionAndBacktracking/Fibonacci.java
@@ -0,0 +1,11 @@
+package RecursionAndBacktracking;
+
+public class Fibonacci {
+    
+    int fib(int n){
+        // base case
+        if(n <= 1) return n;
+        // sum of previous two numbers
+        return fib(n-1) + fib(n-2);
+    }
+}
